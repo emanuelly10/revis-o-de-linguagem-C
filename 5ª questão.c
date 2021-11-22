4bb32904d47a750cf00b185792c88c14c284f497
emanuelly10 <94867320+emanuelly10@users.noreply.github.com>
2021-11-22 16:38:39 -0300

    Add files via upload

---
+++ 5ª questão.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int paes, broas;
+    float total;
+    printf("Digite a quantidade de broas vendidas:\n");
+    scanf("%d", &broas);
+    printf("Digite a quantidade de paes vendidos:\n");
+    scanf("%d", &paes);
+    total = (paes*0.12)+(broas*1.5);
+    printf("O valor total arrecadado foi: %f\n", total);
+    printf("Valor a ser quardado na poupanca é: %f\n", total*0.1);
+
+    return 0;
+}
+
+
